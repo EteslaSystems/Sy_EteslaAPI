d5bed41c2804fb48b14f9606b26a0d025bb76ea9
JesusDCF <jdaniel.cfalcon@gmail.com>
2020-04-15 17:32:31 -0500

    Rutas y metodos faltantes de Usuario

--- src/Controller/usuarioController.js
+++ src/Controller/usuarioController.js
@@ -7,7 +7,7 @@
 const mysqlConnection = require('../../config/database');
 
 function insertarBD (datas) {
-    const { siRol, ttTipoUsuario, vContrasenia, vOficina, vNombrePersona, vPrimerApellido, vSegundoApellido, vTelefono, vEmail, created_at } = datas;
+	const { siRol, ttTipoUsuario, vContrasenia, vOficina, vNombrePersona, vPrimerApellido, vSegundoApellido, vTelefono, vEmail, created_at } = datas;
 
   	return new Promise((resolve, reject) => {
     	mysqlConnection.query('CALL SP_Usuario(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [0, null, null, siRol, ttTipoUsuario, vContrasenia, vOficina, null, vNombrePersona, vPrimerApellido, vSegundoApellido, vTelefono, null, vEmail, created_at, null, null], (error, rows) => {
@@ -16,14 +16,98 @@ function insertarBD (datas) {
 					status: false,
 					message: error
 				}
-
 				resolve (response);
 			} else {
 				const response = {
 					status: true,
 					message: "El registro se ha guardado con éxito."
 				}
+				resolve(response);
+			}
+		});
+  	});
+}
 
+function eliminarBD (datas) {
+	const { idPersona, deleted_at } = datas;
+
+  	return new Promise((resolve, reject) => {
+    	mysqlConnection.query('CALL SP_Usuario(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [1, null, idPersona, null, null, null, null, null, null, null, null, null, null, null, null, null, deleted_at], (error, rows) => {
+			if (error) {
+				const response = {
+					status: false,
+					message: error
+				}
+				resolve (response);
+			} else {
+				const response = {
+					status: true,
+					message: "El registro se ha eliminado con éxito."
+				}
+				resolve(response);
+			}
+		});
+  	});
+}
+
+function editarBD (datas) {
+	const { idPersona, vContrasenia, vOficina, vNombrePersona, vPrimerApellido, vSegundoApellido, updated_at } = datas;
+
+  	return new Promise((resolve, reject) => {
+    	mysqlConnection.query('CALL SP_Usuario(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [2, null, idPersona, null, null, vContrasenia, vOficina, null, vNombrePersona, vPrimerApellido, vSegundoApellido, null, null, null, null, updated_at, null], (error, rows) => {
+			if (error) {
+				const response = {
+					status: false,
+					message: error
+				}
+				resolve (response);
+			} else {
+				const response = {
+					status: true,
+					message: "El registro se ha editado con éxito."
+				}
+				resolve(response);
+			}
+		});
+  	});
+}
+
+function consultaBD () {
+  	return new Promise((resolve, reject) => {
+    	mysqlConnection.query('CALL SP_Usuario(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [3, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null, null], (error, rows) => {
+			if (error) {
+				const response = {
+					status: false,
+					message: error
+				}
+				resolve (response);
+			} else {
+				const response = {
+					status: true,
+					message: rows[0]
+				}
+				resolve(response);
+			}
+		});
+  	});
+}
+
+function consultaIdBD (datas) {
+	const { idPersona } = datas;
+
+  	return new Promise((resolve, reject) => {
+    	mysqlConnection.query('CALL SP_Usuario(?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?, ?)', [6, null, idPersona, null, null, null, null, null, null, null, null, null, null, null, null, null, null], (error, rows) => {
+			if (error) {
+				const response = {
+					status: false,
+					message: error
+				}
+				resolve (response);
+			} else {
+				const response = {
+					status: true,
+					message: rows[0]
+				}
 				resolve(response);
 			}
 		});
@@ -100,6 +184,30 @@ function recuperarPasswordDB(datas) {
 
 module.exports.insertar = async function (datas, response) {
 	const result = await insertarBD(datas);
+
+	return result;
+}
+
+module.exports.eliminar = async function (datas, response) {
+	const result = await eliminarBD(datas);
+
+	return result;
+}
+
+module.exports.editar = async function (datas, response) {
+	const result = await editarBD(datas);
+
+	return result;
+}
+
+module.exports.consultar = async function (response) {
+	const result = await consultaBD();
+
+	return result;
+}
+
+module.exports.consultarId = async function (datas, response) {
+	const result = await consultaIdBD(datas);
 
 	return result;
 }
