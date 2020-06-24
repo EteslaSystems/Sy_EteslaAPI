2070a08577e2928f279357f5757ab90f399ece92
LaloHerA <sistemas.etesla@gmail.com>
2020-06-23 23:07:11 -0500

    Seccion -Tarifas_CFE[Hoja: POWER]- FINALIZADA <> Falta testear los resultados. Se esta elaborando -Pagos_totales[Hoja: POWER]-

--- src/Controller/powerController.js
+++ src/Controller/powerController.js
@@ -41,11 +41,12 @@ module.exports.getProduccionIntermedia_ = async function(data){
 
 //1er. Paso:
 async function getCD_DatosConsumo(data){
+    var objCD = { C:0, D:0 };
     var C = 0;
     var D = 0;
     arrayMeses_ = getIrradiacionDiasDeMesesDelAnio();
 
-    var respuesta = [];
+    var arrayCD = [];
 
     for(var i=0; i<data.arrayPeriodosGDMTH.length; i++)
     {
@@ -63,9 +64,16 @@ async function getCD_DatosConsumo(data){
         console.log('C: '+C+'\nD: '+D);
         console.log('-----------------------------');
 
-        respuesta.push(C,D);
+        objCD = {
+            C: C,
+            D: D
+        }
+
+        arrayCD.push(objCD);
     }
-    return respuesta;
+    
+    getProduccionIntermedia(data, arrayCD)
+
 }
 /*#endregion*/
 
@@ -81,9 +89,9 @@ function getProduccionPunta(){
 }
 
 //2do. paso (2.1) /*Start [este deberia de ser el primer paso]*/
-async function getProduccionIntermedia(data){ //La data debera traer como dato extra "potenciaReal" y "porcentajeDePerdida"
+async function getProduccionIntermedia(data, arrayCD){ //La data debera traer como dato extra "potenciaReal" y "porcentajeDePerdida"
     var produccionIntermedia = 0;
-    var array = [];
+    var __produccionIntermedia = [];
     potenciaReal = data.potenciaReal;
     porcentajePerdida = parseFloat((data.porcentajePerdida)/100);
     arrayMeses_ = getIrradiacionDiasDeMesesDelAnio();
@@ -95,63 +103,19 @@ async function getProduccionIntermedia(data){ //La data debera traer como dato e
 
         produccionIntermedia = potenciaReal * irradicionMes * diasMes * (1 - porcentajePerdida);
         produccionIntermedia = Math.round(produccionIntermedia);
-        array.push(produccionIntermedia);
+        __produccionIntermedia.push(produccionIntermedia);
     }
     console.log('getProduccionIntermedia() says: ');
-    console.log(array);
+    console.log(__produccionIntermedia);
 
-    // return array;
-    /*¿?*/ getBIP_DespuesDeSolar(data, array);
+    getBIP_DespuesDeSolar(data, arrayCD, __produccionIntermedia);
 }
-
-/* function getProduccionSolarIntermedia(_arrayCotizacion, _porcentajePerdida, data){
-    for(var i=0; i<_arrayCotizacion.length; i++){
-        tamanioSystem = _arrayCotizacion[i].paneles.potenciaReal;
-        
-        console.log('Tamanio del sistema: '+tamanioSystem);
-        console.log('-------------------------');
-        _getProduccionIntermedia(tamanioSystem, _porcentajePerdida, data);
-        
-    }
-}
-
-function _getProduccionIntermedia(_tamanioSystem, porcentajePerdida, data){
-    var _produccionAnualKWh = 0;
-    arrayMeses_ = getIrradiacionDiasDeMesesDelAnio();
-    _arrayProduccionIntermedia = [];
-
-    for(var j=0; j<arrayMeses_.length; j++){
-        var _irradiacion = arrayMeses_[j].irradiacion;
-        var _diasMes = arrayMeses_[j].dias;
-        _produccionIntermedia = Math.ceil((_tamanioSystem * _irradiacion * _diasMes * (1 - porcentajePerdida)));
-        
-        //getBIP(data, _produccionIntermedia);
-        _arrayProduccionIntermedia[j] = _produccionIntermedia;
-
-        console.log('_Produccion intermedia: '+_produccionIntermedia);
-        console.log('-------------------------------------------------');
-    
-        _arrayConsumosDespuesDeSolar = _produccionAnualKWh =+ _produccionIntermedia;
-
-        console.log('-----------------------------------------------------------');
-
-        if((j+1) === arrayMeses_.length){
-            _produccionAnualKWh = _produccionAnualKWh - 5;
-            console.log('Produccion anual kWh: '+_produccionAnualKWh);
-            _produccionAnualMWh = Math.ceil(_produccionAnualKWh/1000);
-            console.log('Produccion anual MWh: '+_produccionAnualMWh);
-            console.log('--------------------------------------------------------------------'+'\n'+'\n');
-        }
-    }
-    return _arrayProduccionIntermedia;
-} */
-
 
 /*#endregion*/
 
 /*#region Consumos Despues de Solar*/
 //2do. paso (2.2)
-function getBIP_DespuesDeSolar(data, produccionIntermedia){
+function getBIP_DespuesDeSolar(data, _arrayCD, produccionIntermedia){
     arrayMeses_ = getIrradiacionDiasDeMesesDelAnio();
     var produccionBase = getProduccionBase();
     var produccionPunta = getProduccionPunta();
@@ -201,86 +165,94 @@ function getBIP_DespuesDeSolar(data, produccionIntermedia){
     }
 
     console.log('getBIP_DespuesDeSolar says:\n'+_newBIP);
+
+    getBIPMXN_kWh(data, arrayCD, _newBIP);
 }
-
-
-
-
-
-// var objConsumosDespuesDeSolar = { consumosDespuesDeSolar: { B: 0, I: 0, P: 0, C: 0, D: 0 } };
-// var arrayConsumosDespuesDeSolar = [];
-
-// function getBIP(_data){
-//     for(var i=1; i<_data.length; i++){
-//         var _produccionBase = getProduccionBase(); 
-//         var _produccionPunta = getProduccionPunta();
-
-//         objConsumosDespuesDeSolar = {
-//             consumosDespuesDeSolar: {
-//                 B: _data[i].bkwh - _produccionBase,
-//                 I: _data[i].ikwh - produccionIntermedia_,
-//                 P: _data[i].pkwh - _produccionPunta
-//             }
-//         };
-
-//         arrayConsumosDespuesDeSolar.push(objConsumosDespuesDeSolar);
-//     }
-//     getCD(arrayConsumosDespuesDeSolar, _data);
-//     //console.log('getBIP(_data) says: ');
-//     //console.log(arrayConsumosDespuesDeSolar);
-//     //return arrayConsumosDespuesDeSolar;
-// }
-
-// function getCD(arrayBIP_, data_){
-//     var suma = 0;
-//     var mult = 0;
-//     var _arrayIrrDiasMes = getIrradiacionDiasDeMesesDelAnio();
-//     arrayConsumosDespuesDeSolar = [];
-
-//     for(var x=0; x<arrayBIP_.length; x++){
-//         if((x+1) <= 12)
-//         {
-//             B_ = arrayBIP_[x].consumosDespuesDeSolar.B;
-//             I_ = arrayBIP_[x].consumosDespuesDeSolar.I;
-//             P_ = arrayBIP_[x].consumosDespuesDeSolar.P;
-
-//             pKw = data_[x+1].pkw;
-
-//             suma = B_ + I_ + P_;
-//             mult = 24 * _arrayIrrDiasMes[x].dias * 0.57;
-//             issue = suma / mult;
-//             resultC = Math.ceil(Math.min(pKw, issue));
-//             /*---- D ----*/
-//             bKw_ = data_[x+1].bkw;
-//             iKw_ = data_[x+1].ikw;
-//             pKw_ = data_[x+1].pkw;
-//             _Dmaximo = Math.max(bKw_, iKw_, pKw_);
-//             resultD = Math.ceil(Math.min(issue, _Dmaximo));
-            
-//             objConsumosDespuesDeSolar = {
-//                 consumosDespuesDeSolar: {
-//                     B: B_,
-//                     I: I_,
-//                     P: P_,
-//                     C: resultC,
-//                     D: resultD,
-//                 }
-//             };
-
-//             arrayConsumosDespuesDeSolar.push(objConsumosDespuesDeSolar);
-//         }else{
-//             break;
-//         }
-//     }
-//     console.log('getCD(arrayBIP_, data_) says: ');
-//     console.log(arrayConsumosDespuesDeSolar);
-// }
-/*#endregion*/
-
-/*#region Pagos_totales*/
 /*#endregion*/
 
 /*#region Tarifas_CFE*/
+//3er. Paso
+function getBIPMXN_kWh(data, arrayCD, newBIP){
+    var objBIPMXN_kwh = {};
+    var _bipMXN_kWh = [];
+
+    for(var i=0; i<data.arrayPeriodosGDMTH.length; i++)
+    {
+        var bkwh = parseFloat(data.arrayPeriodosGDMTH[i].bkwh);
+        var ikwh = parseFloat(data.arrayPeriodosGDMTH[i].ikwh);
+        var pkwh = parseFloat(data.arrayPeriodosGDMTH[i].pkwh);
+        var bmxn = parseFloat(data.arrayPeriodosGDMTH[i].bmxn);
+        var imxn = parseFloat(data.arrayPeriodosGDMTH[i].imxn);
+        var pmxn = parseFloat(data.arrayPeriodosGDMTH[i].pmxn);
+        var cmxn = parseFloat(data.arrayPeriodosGDMTH[i].cmxn);
+        var dmxn = parseFloat(data.arrayPeriodosGDMTH[i].dmxn);
+
+        var ckw = arrayCD[i].objCD.C;
+        var dkw = arrayCD[i].objCD.D;
+
+        var newB = newBIP[i].newB;
+        var newI = newBIP[i].newI;
+        var newP = newBIP[i].newP;
+
+        bmxn_kwh = bmxn / bkwh;
+        imxn_kwh = imxn / ikwh;
+        pmxn_kwh = pmxn / pkwh;
+        cmxn_kw = cmxn / ckw;
+        dmxn_kw = dmxn / dkw;
+
+        energiaFaltante = newB + newI + newP;
+
+        objBIPMXN_kwh = {
+            bmxn_kwh: bmxn_kwh,
+            imxn_kwh: imxn_kwh,
+            pmxn_kwh: pmxn_kwh,
+            cmxn_kw: cmxn_kw,
+            dmxn_kw: dmxn_kw,
+            energiaFaltante: energiaFaltante
+        }
+
+        _bipMXN_kWh.push(objBIPMXN_kwh);
+    }
+    console.log('getBIPMXN_kWh:\n'+_bipMXN_kWh);
+
+    getPagosTotales(data, _bipMXN_kWh, arrayCD, newBIP);
+}
+/*#endregion*/
+
+/*#region Pagos_totales*/
+//4to. paso
+function getPagosTotales(data, bipMXN_kWh, arrayCD, newBIP){
+    var objPagosTotales = {};
+    /*#region SIN_SOLAR*/
+    for(var i=0; i<data.arrayPeriodosGDMTH.length; i++)
+    {
+        objPagosTotales = {
+            sinSolar: {
+                energia:,
+                capacidad:,
+                distribucion:,
+                iva:,
+                total:
+            }
+        };
+    }
+    /*#endregion*/
+    /*#region CON_SOLAR*/
+    for(var j=0; j<data.arrayPeriodosGDMTH.length; j++)
+    {
+        objPagosTotales = {
+            conSolar: {
+                energia:,
+                transimision:,
+                capacidad:,
+                distribucion:,
+                iva:,
+                total:
+            }
+        };
+    }
+    /*#endregion*/
+}
 /*#endregion*/
 
 /*#region Celdas_Arriba(Radiacion, Produccion_Anual(kWh), Produccion_Anual(mWh), Total_SinSolar, Total_ConSolar, Ahorro, Porcentaje)*/
