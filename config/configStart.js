5960d078e5953cdf772120f289ae31c71030e53f
LaloHerA <sistemas.etesla@gmail.com>
2020-07-16 23:05:17 -0500

    x

--- config/configStart.js
+++ config/configStart.js
@@ -7,17 +7,16 @@
 
 /* const compression = require('compression'); */ //Compression GZIP - Necesaria
 const express = require('express'); //Constante que instancia la libreria 'express'.
-const app = express(); //Constante encargada de crear el punto de partida del servidor.
-
 const http = require('http');
 
+const app = express(); //Constante encargada de crear el punto de partida del servidor.
+const server = http.createServer(app);
+
 /* app.use(compression); */
-app.use(express.json()); //Se le indica al servidor que la salida de la comunicación sera a través de un objeto json.
 app.use(require('../src/Routes/web.js')); //Instancia del archivo de rutas del servidor.
 app.set('port', process.env.PORT || 3000); //Esta variable obtiene el numero de puerto que queramos poner, si no recibe nada le asigna el valor '3000'.
 
-app.listen(app.get('port'), () => { //Se da inicio al servidor con los parametros antes declarados.
+server.listen(app.get('port'), () => { //Se da inicio al servidor con los parametros antes declarados.
     console.log('Servidor iniciado de manera exitosa en el puerto: ', app.get('port'));
 });
 
-const server = http.createServer(app);
