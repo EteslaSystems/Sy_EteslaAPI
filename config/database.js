08ffe4784de16a978904b5e25624d4a52af93e9d
LH420 <sistemas.etesla@gmail.com>
2022-01-26 18:06:52 +0000

    s

--- config/database.js
+++ config/database.js
@@ -8,22 +8,11 @@
 const mysql = require ('mysql'); //Constante que instancia la dependencia de MySQL
 
 const mysqlConnection = mysql.createConnection({ //Cadena de conexión a la base de datos de MySQL
-<<<<<<< HEAD
 	host: 'localhost',
 	user: 'esclavo',
         //user: root,
 	password: '$!Etesla123$',
 	database: 'eteslapanelessolares_sy'
-=======
-	host: '137.184.97.127',
-	user: 'esclavo',
-	password: '$!Etesla123$',
-	database: 'eteslapanelessolares_sy',
-	/* ---------------------------------------- */
-	// host: 'localhost',
-	// user: 'root',
-	// database: 'eteslapanelessolares_sy'
->>>>>>> 96f656f4d6d597cfcf2b6a3560b41dfb96149019
 });
 
 mysqlConnection.connect(function (err){ //función que manda un mensaje a la consola con el resultado de la conección, ya sea exitosa o no
